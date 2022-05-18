de7abfd7edff9e8201069a43f5a6250794d35631
Sergio <sergio.quiroz2012@gmail.com>
2022-05-17 21:30:24 -0700

    adding button

--- src/components/Semester.js
+++ src/components/Semester.js
@@ -62,6 +62,13 @@ class Semester extends Component {
                 variant="outlined" color="primary" style={{margin: 10}}>
                 Get Schedule
               </Button>
+
+              
+              <Button component={Link} 
+                    to={{pathname:'/addStudent'}}
+                variant="outlined" color="primary" style={{margin: 10}}>
+              Add new Student </Button>
+              
           </div>
       </div>
     )
